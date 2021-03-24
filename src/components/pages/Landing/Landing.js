49c2fdba9d0c159f8f51f2446bc61424d1ad7cd4
unknown <nandhinimadan20@gmail.com>
2021-03-23 22:23:50 -0700

    Moved the search bars[zipcode & category search] from body to top

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -2,24 +2,30 @@ import React from 'react';
 import MainNavBar from '../../common/mainNavBar';
 import './landing.css';
 import BrowserBar from '../../common/browserBar';
-
+import Top1 from '../Landing/Top1.jpg';
+import Top2 from '../Landing/Top2.jpg';
+import tittle_img from '../Landing/Top3.jpg';
+import Top3 from '../Landing/Top4.jpg';
+import logo from '../Landing/marketplace-logo.png';
 const Landing = () => {
   return (
-    <div>
+    <div className="container">
       <MainNavBar />
-      <section className="browse">
-        <h1 className="title-1">Browse your favorite local store here!</h1>
-        <h1>Support your community's business!</h1>
-        <div className="browse-bar">
-          {' '}
-          <BrowserBar />
-        </div>
-      </section>
+
+      <div>
+        <img className="tittle_img" src={tittle_img}></img>
+      </div>
       <h1 className="title-2">Top rate merchants</h1>
       <section className="top-rated">
-        <div className="top-img">1</div>
-        <div className="top-img">2</div>
-        <div className="top-img">3</div>
+        <div className="top-img">
+          <img src={Top1}></img>
+        </div>
+        <div className="top-img">
+          <img src={Top2}></img>
+        </div>
+        <div className="top-img">
+          <img src={Top3}></img>
+        </div>
       </section>
     </div>
   );
