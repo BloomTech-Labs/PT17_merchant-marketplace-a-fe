8aef7ce4a4d5d9830889171113c515e64cff827e
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2021-03-25 19:48:49 -0400

    added link to add profile image

--- src/components/sellerPages/profile/dashboard/index.js
+++ src/components/sellerPages/profile/dashboard/index.js
@@ -7,6 +7,13 @@ import CustomerSection from './customerSection';
 function Dashboard() {
   return (
     <div className="dashboard">
+      <img
+        class="profile-pic"
+        src="http://cdn.cutestpaw.com/wp-content/uploads/2012/07/l-Wittle-puppy-yawning.jpg"
+      />
+      <div class="upload-button">Upload Image</div>
+      <input class="file-upload" type="file" accept="image/*" />
+
       <div className="dashItem">
         <InvSection />
       </div>
