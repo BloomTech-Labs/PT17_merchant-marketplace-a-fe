314b9653a740ea641aa6b67866fd990b71a71d45
unknown <nandhinimadan20@gmail.com>
2021-04-20 20:47:18 -0700

    created Product search component

--- src/components/pages/ProductSearch/index.js
+++ src/components/pages/ProductSearch/index.js
@@ -3,60 +3,51 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-
+import { Row, Col } from 'antd';
+import NotFound from '../NotFound';
 import { Card } from 'antd';
 import { useOktaAuth } from '@okta/okta-react';
-import { getProductsData } from '../../../api';
-//This component is to display results of products
-
+import { getProductsData, search } from '../../../api';
+import './ProductSearch.css';
 const { Meta } = Card;
 
 const ProductSearch = () => {
   const location = useLocation();
-  const term = location.state.detail;
+  const term = location.state.searchItem;
   const { authState, authService } = useOktaAuth();
   const [productResult, setProductResult] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(term);
-  //backend
+  const [zipcode, setzipcode] = useState('10001');
+
   useEffect(() => {
-    getProductsData(authState)
+    axios
+      .get(`http://localhost:8000/search/?title=${term}&zip=${zipcode}`)
       .then(response => {
         setProductResult(response.data);
-        console.log(response, 'resp');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
 
+        console.log(response.data, 'Useeffect');
+      });
+  }, [term]);
   return productResult.length >= 1 ? (
-    <div className="productResult">
-      {productResult
-        .filter(productResult =>
-          productResult.item_name.toLowerCase().includes(searchTerm)
-        )
-        .map(product => (
-          <div key={product.id}>
+    <div className="productResult_container">
+      {productResult.map(product => (
+        <div className="productResult" key={product.id}>
+          {
             <Card
               hoverable
               style={{ width: 240 }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                />
-              }
+              cover={<img alt="example" src={product.images[0]} />}
             >
               <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
+                title={product.item_name}
+                description={product.description}
               />
             </Card>
-          </div>
-        ))}
+          }
+        </div>
+      ))}
     </div>
   ) : (
-    <h2>Product Not Found</h2>
+    <h2>NOt found</h2>
   );
 };
 export default ProductSearch;
