47b952a85689871eb67b05e7025f976970cf20c6
Miley Wright <dishaliekwright@yahoo.com>
2021-04-26 16:06:03 -0400

    marketing container

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './landing.css';
 import FeaturedProduct from './FeaturedProduct';
-// import {getExampleData} from '../../../api'; //will be replaced with api
+import { Link } from 'react-router-dom';
 import logo from '../Landing/banner.png';
+import selling from '../Landing/s-l960.webp';
 import axios from 'axios';
 
 const Landing = () => {
@@ -22,6 +23,27 @@ const Landing = () => {
       <div className="banner">
         <img src={logo} className="banner_img" />
       </div>
+
+      <div className="selling">
+        <div className="selling_text">
+          <h1>
+            Buy, sell, and discover antique and vintage, home decor, and more
+          </h1>
+          <p>
+            Sign up now and join millions of people on the social marketplace
+            for all things style
+          </p>
+          <Link to="/login">
+            <button>Sign Up</button>
+          </Link>
+        </div>
+        <img
+          src={selling}
+          className="selling_img"
+          style={{ width: '1900px' }}
+        />
+      </div>
+
       <div className="featured_container">
         <h1 className="title">Featured Listings</h1>
         <div className="feature_grid">
